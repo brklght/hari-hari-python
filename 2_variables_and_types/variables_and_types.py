0f0f27b0d4d98049b5bc0f0866515338c361be82
brklght <lighterland@gmail.com>
2022-04-25 13:16:31 +0300

    update

--- 2_variables_and_types/variables_and_types.py
+++ 2_variables_and_types/variables_and_types.py
@@ -1,1 +1,22 @@
-x = 1
+Boolean = [True, False]
+String = "This is string"
+Integer = 1
+Float = 1.11
+Hexadecimal = hex(0x1a)
+Complex = 1 + 1j
+List = [Boolean, String, Integer]
+Tuple = (Boolean, String, Integer)
+Dictionary =	{
+				'Boolean': 'True/False',
+				'Integer': 1
+				}
+
+print(Boolean)
+print(String)
+print(Integer)
+print(Float)
+print(Hexadecimal)
+print(Complex)
+print(List)
+print(Tuple)
+print(Dictionary)
